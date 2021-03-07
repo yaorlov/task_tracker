84f876716e8657b019e269369fb37aa0dbc3b23e
Yuriy Orlov <orlov1991@gmail.com>
2021-03-07 15:10:49 +0200

    Add tasks page for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@
 
 Rails.application.routes.draw do
   resources :tasks, only: [:index]
+
+  # resources :user_accounts, as: :users do
+  #   member do
+  #     get :tasks
+  #   end
+  # end
+  get '/users/:user_account_id/tasks', to: 'user_accounts#tasks'
 end
